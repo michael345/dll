facb228cf5fd15986ecc67acda6cf99e17cc540d
michael345 <michael345@gmail.com>
2021-01-03 01:13:53 +0200

    added ifdef to dll.h

--- dll.h
+++ dll.h
@@ -1,3 +1,6 @@
+#ifndef __DLL__
+#define __DLL__
+
 typedef struct dll_node_{
 
         void *data;
@@ -6,7 +9,7 @@ typedef struct dll_node_{
 } dll_node_t;
 
 typedef struct dll_{
-        dll_node_t_ *head;
+        dll_node_t *head;
 } dll_t;
 
 dll_t *
@@ -17,3 +20,5 @@ add_data_to_dll (dll_t *dll, void *app_data);
 
 int
 add_data_to_end_of_dll (dll_t *dll, void *app_data);
+
+#endif
